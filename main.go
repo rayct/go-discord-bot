eb130761a27001bf0978811794d79ac5edbea2a8
rayct <rayct@icloud.com>
2024-02-17 22:49:12 +0000

    Revision: Feb 17th, 2024 | Added Multi-Plexer(mux)

--- main.go
+++ main.go
@@ -2,47 +2,47 @@ package main
 
 import (
 	"GoDiscordBot/config"
+	"GoDiscordBot/mux"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	BotID string
-	bot   *discordgo.Session
+	bot    *discordgo.Session
+	Router *mux.Mux
 )
 
-func Start() {
+func init() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	bot, err := discordgo.New("Bot " + config.Token)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	bot.AddHandler(messageHandler)
+	Router = mux.New()
 
-	err = bot.Open()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	BotID = bot.State.User.ID
-
-	fmt.Println("Bot is now running!")
+	bot.AddHandler(Router.OnMessageCreate)
 
 }
 
 func main() {
-	err := config.ReadConfig()
+	err := bot.Open()
+
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	Start()
+	fmt.Println("Bot is now running!")
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
@@ -51,36 +51,36 @@ func main() {
 	bot.Close()
 }
 
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == BotID {
-		return
-	}
+// func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+// 	if m.Author.ID == BotID {
+// 		return
+// 	}
 
-	if m.Content == config.BotPrefix+" ping" {
-		_, err := s.ChannelMessageSend(m.ChannelID, "Hello! "+(time.Since(m.Timestamp)).String())
+// 	if m.Content == config.BotPrefix+" ping" {
+// 		_, err := s.ChannelMessageSend(m.ChannelID, "Hello! "+(time.Since(m.Timestamp)).String())
 
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 	}
 
-	if m.Content == config.BotPrefix+" embed" {
-		embed := &discordgo.MessageEmbed{}
-		embed.Title = "This is an embed"
-		embed.Color = 1752220
-		embed.Description = "This is a description"
+// 	if m.Content == config.BotPrefix+" embed" {
+// 		embed := &discordgo.MessageEmbed{}
+// 		embed.Title = "This is an embed"
+// 		embed.Color = 1752220
+// 		embed.Description = "This is a description"
 
-		// embed.Video = &discordgo.MessageEmbedVideo{}
-		// embed.Video.URL = "https://gifdb.com/images/high/singer-rick-astley-never-gonna-give-you-up-bfkvglwju2mte6ff.gif"
+// 		// embed.Video = &discordgo.MessageEmbedVideo{}
+// 		// embed.Video.URL = "https://gifdb.com/images/high/singer-rick-astley-never-gonna-give-you-up-bfkvglwju2mte6ff.gif"
 
-		embed.Thumbnail = &discordgo.MessageEmbedThumbnail{}
-		embed.Thumbnail.URL = "https://hips.hearstapps.com/hmg-prod/images/Emma-Watson_GettyImages-619546914.jpg?crop=1xw:1.0xh;center,top&resize=640:*"
+// 		embed.Thumbnail = &discordgo.MessageEmbedThumbnail{}
+// 		embed.Thumbnail.URL = "https://hips.hearstapps.com/hmg-prod/images/Emma-Watson_GettyImages-619546914.jpg?crop=1xw:1.0xh;center,top&resize=640:*"
 
-		_, err := s.ChannelMessageSendEmbed(m.ChannelID, embed)
+// 		_, err := s.ChannelMessageSendEmbed(m.ChannelID, embed)
 
-		if err != nil {
-			fmt.Println(err)
-		}
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
 
-	}
-}
+// 	}
+// }
