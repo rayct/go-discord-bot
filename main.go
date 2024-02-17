ba2602135b393928ca366da26972a41a781bc5c3
rayct <rayct@icloud.com>
2024-02-17 22:13:28 +0000

    Added Image and Description Embed

--- main.go
+++ main.go
@@ -70,6 +70,9 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		embed.Color = 1752220
 		embed.Description = "This is a description"
 
+		// embed.Video = &discordgo.MessageEmbedVideo{}
+		// embed.Video.URL = "https://gifdb.com/images/high/singer-rick-astley-never-gonna-give-you-up-bfkvglwju2mte6ff.gif"
+
 		embed.Thumbnail = &discordgo.MessageEmbedThumbnail{}
 		embed.Thumbnail.URL = "https://hips.hearstapps.com/hmg-prod/images/Emma-Watson_GettyImages-619546914.jpg?crop=1xw:1.0xh;center,top&resize=640:*"
 
